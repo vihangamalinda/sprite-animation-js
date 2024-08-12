dea5c14103c478b002bc3c730ed99e7dae1051cc
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2024-08-12 10:56:20 -0800

    feat:Adding drop down to dynamically select the type of the animation.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,9 @@
+let type ='fall';
+const dropDown = document.getElementById("animations");
+addEventListener('change',(event)=>{
+    type =event.target.value;
+})
+
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 console.log(ctx);
@@ -14,11 +20,73 @@ playerImage.src = "../assets/shadow_dog.png";
 const spriteWidth = 575;
 const spriteHeight = 523;
 
-let frameX = 0;
-let frameY = 0;
 
-let gameFrame =0;
-const staggerFrames =5;
+// let frameX = 0;
+// let frameY = 0;
+
+let gameFrame = 0;
+const staggerFrames = 5;
+
+const spriteAnimations = [];
+
+const animationState = [
+  {
+    name: "idle",
+    frames: 7,
+  },
+  {
+    name: "jump",
+    frames: 7,
+  },
+  {
+    name: "fall",
+    frames: 7,
+  },
+  {
+    name: "run",
+    frames: 9,
+  },
+  {
+    name: "dizzy",
+    frames: 11,
+  },
+  {
+    name: "sit",
+    frames: 5,
+  },
+  {
+    name: "roll",
+    frames: 7,
+  },
+  {
+    name: "bite",
+    frames: 7,
+  },
+  {
+    name: "ko",
+    frames: 12,
+  },
+  {
+    name: "getHit",
+    frames: 7,
+  },
+];
+
+animationState.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  };
+
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * spriteWidth;
+    let positionY = index;
+
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+  spriteAnimations[state.name] = frames;
+});
+
+console.log(spriteAnimations);
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -35,9 +103,19 @@ function animate() {
   //     destination_height
   //   );
 
+//   let type ='fall';
+
+  let postion =
+    Math.floor(gameFrame / staggerFrames) % spriteAnimations[type].loc.length;
+
+//   let frameX = spriteWidth * postion;
+    let frameX = spriteAnimations[type].loc[postion].x;
+
+  let frameY = spriteAnimations[type].loc[postion].y;
+
   ctx.drawImage(
     playerImage,
-    frameX * spriteWidth,
+    frameX,
     frameY * spriteHeight,
     spriteWidth,
     spriteHeight,
@@ -47,12 +125,14 @@ function animate() {
     spriteHeight
   );
 
-  if(gameFrame% staggerFrames ===0){
-  if (frameX < 6) frameX++;
-  else frameX = 0;
-  }
-  console.log(gameFrame)
+  //   if (gameFrame % staggerFrames === 0) {
+  //     if (frameX < 6) frameX++;
+  //     else frameX = 0;
+  //   }
+  //   console.log(gameFrame)
   requestAnimationFrame(animate); //recurssion
   gameFrame++;
 }
 animate();
+
+
